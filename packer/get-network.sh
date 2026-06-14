9143fec606a1728b9b3dee5320f80cf7c5d3d16a
danil <dannsk@inbox.ru>
2026-06-15 02:33:10 +0700

    update2

--- packer/get-network.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-NETWORK_ID=$(openstack network show internet -c id -f value 2>/dev/null || openstack network list -c ID -c Name -f value | grep internet | awk '{print $1}')
-echo "network_id = \"$NETWORK_ID\"" > network.auto.pkrvars.hcl
-echo "Found network: $NETWORK_ID"
