988721e529eaedc196213dc306b80ca888fcd113
danil <dannsk@inbox.ru>
2026-06-15 04:50:15 +0700

    1

--- packer/image-name.sh
+++ packer/image-name.sh
@@ -1,10 +1,18 @@
 #!/bin/bash
-# Получить ID последнего образа Packer
-IMAGE_ID=$(openstack image list -f value -c ID -c Name | grep web-server-base | sort -k2 | tail -1 | awk '{print $1}')
+# Получить токен
+TOKEN=$(curl -s -X POST https://msk.cloud.vk.com/infra/identity/v3/auth/tokens \
+  -H "Content-Type: application/json" \
+  -d "{\"auth\":{\"identity\":{\"methods\":[\"password\"],\"password\":{\"user\":{\"name\":\"$OS_USERNAME\",\"domain\":{\"name\":\"$OS_USER_DOMAIN_NAME\"},\"password\":\"$OS_PASSWORD\"}}},\"scope\":{\"project\":{\"id\":\"$OS_PROJECT_ID\"}}}}" \
+  -i | grep "X-Subject-Token" | cut -d" " -f2- | tr -d '\r')
 
-if [ -z "$IMAGE_ID" ]; then
-  echo "Образ не найден!"
+# Получить последний образ
+IMAGE_ID=$(curl -s -H "X-Auth-Token: $TOKEN" \
+  "https://infra.mail.ru:9292/v2/images?name=web-server-base&sort=created_at:desc&limit=1" | \
+  python3 -c "import sys,json; data=json.load(sys.stdin); print(data['images'][0]['id'] if data['images'] else 'none')")
+
+if [ "$IMAGE_ID" != "none" ]; then
+  echo "image_name = \"$IMAGE_ID\"" > terraform/image.auto.tfvars
+  echo "Found image: $IMAGE_ID"
 else
-  echo "Найден образ: $IMAGE_ID"
-  echo "image_name = \"$IMAGE_ID\"" > image.auto.tfvars
+  echo "No image found!"
 fi
